a3080d6382bd4613eafb4066d2e544a65a749265
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-01-30 20:53:19 +0100

    t

--- mongo-collection/src/containers/CreateUser.js
+++ mongo-collection/src/containers/CreateUser.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import CreateUserForm from '../components/CreateUserForm';
 
 
-
 export default class CreateUser extends Component {
     constructor(props) {
         super(props); 
@@ -53,16 +52,15 @@ export default class CreateUser extends Component {
             person_name: this.state.person_name,
             person_age: this.state.person_age,
             person_movie: this.state.person_movie,
-            person_gender: this.state.person_gender
-            
+            person_gender: this.state.person_gender    
         };
+
         axios.post('http://localhost:5000/celebrity/add', obj)
         .then(res => console.log(res.data))
         .catch((err) => {
             console.log("Something went wrong", err);
         }) ;
-        
-        
+         
         this.setState({
           person_name: '',
           person_age: '',
