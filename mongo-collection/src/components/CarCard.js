19030256f7446af0e526c4a001dbb50f0b3fe215
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-01-30 20:57:12 +0100

    link

--- mongo-collection/src/components/CarCard.js
+++ mongo-collection/src/components/CarCard.js
@@ -5,7 +5,8 @@ class CarCard extends Component {
   render() {
     return (
         <div className="card" >
-            <img className="card-img-top" src="" alt="" />
+            <img className="card-img-top" src="https://www.deerpassgolf.com/wp-content/uploads/2018/12/volkswagen-golf-vw-golf-4.jpg"
+             alt="" style={{width : 200, height: 150, marginTop : 10}} />
                 <div className="card-body">
                     <h5 className="card-title">Brand: {this.props.obj.car_brand}</h5>
                     <p className="card-text">Year: {this.props.obj.car_year}</p>
