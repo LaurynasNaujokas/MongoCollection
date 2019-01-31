a7fa2684182ff6b0003728c08964a817eb4b9e67
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-01-31 21:30:55 +0100

    Leave only cars

--- mongo-collection/src/App.js
+++ mongo-collection/src/App.js
@@ -4,9 +4,8 @@ import 'bulma/css/bulma.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import CreateUser from './containers/CreateUser';
 import CreateCar from './containers/CreateCar';
-import Home from './components/Home';
+import Home from './containers/Home';
 
 class App extends Component {
   render() {
@@ -14,14 +13,11 @@ class App extends Component {
     <Router>
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to={'/'} className="navbar-brand">Celebrities</Link>
+            <Link to={'/'} className="navbar-brand">Cars</Link>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                   <Link to={'/'} className="nav-link">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/CreateUser'} className="nav-link">Create Celebrity</Link>
                 </li>
                 <li className="nav-item">
                   <Link to={'/CreateCar'} className="nav-link">Add New Car</Link>
@@ -29,10 +25,9 @@ class App extends Component {
               </ul>
             </div>
           </nav> <br/>
-          <h2 className="title is-3-is-spaced">Celebrities</h2> <br/>
+          <h2 className="title is-3-is-spaced">All Cars</h2> <br/>
           <Switch>
               <Route exact path='/' component={ Home } />
-              <Route exact path='/createUser' component={ CreateUser } /> 
               <Route exact path='/createCar' component={ CreateCar } /> 
           </Switch>
         </div>
--- mongo-collection/src/containers/Home.js
+++ mongo-collection/src/containers/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import TableRow from '../components/TableRow';
+
 import CarCard from '../components/CarCard';
 
 
@@ -8,34 +8,17 @@ export default class CreateUser extends Component {
     constructor(props){
         super(props);
         this.state = {
-            celebrity : [],
             car : []
         };
     }
 
     componentDidMount(){
-        axios.get('http://localhost:5000/celebrity')
-        .then(response => {
-            this.setState({celebrity: response.data});
-        })
-        .catch(function (err) {
-            console.log("Something went wrong", err);
-        });
-
-
         axios.get('http://localhost:5000/car')
         .then(response => {
             this.setState({car: response.data});
         })
         .catch(function (err) {
             console.log("Something went wrong", err);
-        });
-    }
-
-    
-    tabRow() {
-        return this.state.celebrity.map(function(object, i ){
-            return <TableRow obj={object} key={i} />;
         });
     }
 
@@ -47,28 +30,12 @@ export default class CreateUser extends Component {
 
     render() {
         return (
-            <div>
-              <h3 align="center">Celebrity List</h3>
-                <table className="table table-striped" style={{marginTop : 20}}>
-                    <thead>
-                        <tr>
-                        <th>Name</th>
-                        <th>Movie</th>
-                        <th>Age</th>
-                        <th>Gender</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { this.tabRow() }
-                    </tbody>
-                </table>
                 <div>
                     <h2 align="center">Cars</h2>
                     <div>
                         { this.carDetails() }
                     </div>
                 </div>
-            </div>
            
         )
     }
