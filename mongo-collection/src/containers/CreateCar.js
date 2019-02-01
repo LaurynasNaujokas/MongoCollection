bf5f4f238fae19ab69ff3dbd395cfd25a67a8221
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-02-01 16:34:04 +0100

    Update CreateCar.js

--- mongo-collection/src/containers/CreateCar.js
+++ mongo-collection/src/containers/CreateCar.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import CarForm from '../components/CarForm';
+//import CarForm from '../components/CarForm';
 
 export default class CreateCar extends Component {
     constructor(props) {
@@ -82,7 +82,55 @@ export default class CreateCar extends Component {
     render() {
         return (
             <div >
-              <CarForm />
+              <div>
+             <h3 style={{ marginBottom: 10 }}>Add New Car: </h3>
+              <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                      <label>Car Brand:</label>
+                      <input 
+                        type="text" 
+                        className="form-control" 
+                        value={this.props.car_brand}
+                        onChange={this.onChangeCarBrand}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>Car Year: </label>
+                      <input type="text" 
+                        className="form-control"
+                        value={this.props.car_year}
+                        onChange={this.onChangeCarYear}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>Car Color: </label>
+                      <input type="text" 
+                        className="form-control"
+                        value={this.props.car_color}
+                        onChange={this.onChangeCarColor}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>About car: </label>
+                      <input type="text" 
+                        className="form-control"
+                        value={this.props.car_about}
+                        onChange={this.onChangeCarAbout}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>Car price: </label>
+                      <input type="text" 
+                        className="form-control"
+                        value={this.props.car_price}
+                        onChange={this.onChangeCarPrice}
+                        />
+                  </div> 
+                  <div className="form-group">
+                      <input type="submit" value="Add New Car" className="btn btn-primary"/>
+                  </div>
+              </form>
+        </div>
           </div>
         )
     }
