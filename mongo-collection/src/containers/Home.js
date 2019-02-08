6f966f58fdfb301804cb3e70245138b7cf3efaf6
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-02-08 10:03:53 +0100

    Update Home.js

--- mongo-collection/src/containers/Home.js
+++ mongo-collection/src/containers/Home.js
@@ -34,11 +34,11 @@ render(){
       {this.renderList()}
       <nav aria-label="Page navigation example" style={{marginTop : 50, marginLeft : 400}}>
         <ul className="pagination">
-            <li className="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li className="page-item"><a class="page-link" href="#">1</a></li>
-            <li className="page-item"><a class="page-link" href="#">2</a></li>
-            <li className="page-item"><a class="page-link" href="#">3</a></li>
-            <li className="page-item"><a class="page-link" href="#">Next</a></li>
+            <li className="page-item"><a class="page-link" href="">Previous</a></li>
+            <li className="page-item"><a class="page-link" href="">1</a></li>
+            <li className="page-item"><a class="page-link" href="">2</a></li>
+            <li className="page-item"><a class="page-link" href="">3</a></li>
+            <li className="page-item"><a class="page-link" href="">Next</a></li>
         </ul>
     </nav>
     </ul>
