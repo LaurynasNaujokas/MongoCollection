f0a7741964422fa41feef6aab375376ce4d17f0b
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-02-20 14:42:32 +0100

    Update Navigation.js

--- mongo-collection/src/components/Navigation.js
+++ mongo-collection/src/components/Navigation.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-
 import Home from '../containers/Home';
 import CreateCar from '../containers/CreateCar';
 import CarDetails from '../components/CarDetails';
   
-
 class Navigation extends Component {
   render() {
     return (
